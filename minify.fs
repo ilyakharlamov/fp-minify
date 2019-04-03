2f788e992d51f23dce6862be43a3772df39f8423
Ilya Kharlamov <502372+ilyakharlamov@users.noreply.github.com>
2019-04-03 17:26:35 -0400

    Echo the file content without the changes

--- minify.fs
+++ minify.fs
@@ -1,8 +1,17 @@
+let rec minify lines = 
+    match Seq.length lines with
+    | 0 -> ""
+    | _ ->
+        let currentline = Seq.head lines
+        currentline + (Seq.tail lines |> minify)
+
 [<EntryPoint>]
 let main argv = 
     match argv.Length with
     | 1 -> 
-        printfn "you asked to minify file %s" argv.[0]
+        seq { yield! System.IO.File.ReadAllLines argv.[0]}
+            |> minify
+            |> printfn "%s"
         0
     | _ -> 
         printfn "Expecting only one argument"
