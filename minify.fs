8ccd88e7fe992e07d304a3a3b0aa75bb417780f0
Ilya Kharlamov <502372+ilyakharlamov@users.noreply.github.com>
2019-04-03 17:38:35 -0400

    final version that replaces up to 26 variable names

--- minify.fs
+++ minify.fs
@@ -1,16 +1,27 @@
-let rec minify lines = 
+let alphanumerics = System.Text.RegularExpressions.Regex("[a-zA-Z][\\w\\d]*")
+
+let keywords = set ["var";  "function"; "return"; "if"; "else"]
+
+let rec minify varreplacements lines = 
     match Seq.length lines with
     | 0 -> ""
     | _ ->
-        let currentline = Seq.head lines
-        currentline + (Seq.tail lines |> minify)
+        let (currentline:string) = Seq.head lines
+        let varreplacements' = seq { for x in alphanumerics.Matches currentline -> x.Value }
+                                |> Seq.filter (fun oldname -> not(Map.containsKey oldname varreplacements || Seq.contains oldname keywords))
+                                |> Seq.fold (fun sofar oldname -> Map.add oldname (Map.count sofar + 97 |> char |> string) sofar) varreplacements
+        let newname oldname = 
+            match Map.tryFind oldname varreplacements' with
+            | Some x -> x
+            | None -> oldname
+        alphanumerics.Replace(currentline, fun mtch -> newname mtch.Value) + (Seq.tail lines |> minify varreplacements')
 
 [<EntryPoint>]
 let main argv = 
     match argv.Length with
     | 1 -> 
         seq { yield! System.IO.File.ReadAllLines argv.[0]}
-            |> minify
+            |> minify Map.empty
             |> printfn "%s"
         0
     | _ -> 
